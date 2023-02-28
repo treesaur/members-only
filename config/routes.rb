b3b59276ceea4c8f578ba9afc6829fa22c3b1ae7
rly <rallenly@gmail.com>
2023-02-27 19:40:10 -0500

    Shows author only if signed in and links routed to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:new, :create, :index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
